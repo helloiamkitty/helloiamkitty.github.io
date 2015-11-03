8635448cc762b39c1c5dba65cbf34d30bd56d695
helloiamkitty <zk_liu@qq.com>
2015-11-03 23:45:40 +0800

    更新背景颜色

--- js/xiaoqiu/weixinshare.js
+++ js/xiaoqiu/weixinshare.js
@@ -1,5 +1,5 @@
 
-var shareInfo={appid:'',imgUrl:'http://qzonestyle.gtimg.cn/aoi/sola/20141009162923_NXt0OmYzxt.png',lineLink:'http://qzonestyle.gtimg.cn/touch_proj/proj-pad-report/index.html',descContent:'2014腾讯 中国平板电脑用户行为报告',shareTitle:'中国平板电脑用户行为报告 - 腾讯ISUX'}
+var shareInfo={appid:'',imgUrl:'http://qzonestyle.gtimg.cn/aoi/sola/20141009162923_NXt0OmYzxt.png',lineLink:'http://qzonestyle.gtimg.cn/touch_proj/proj-pad-report/index.html',descContent:'小邱港货2周年啦！',shareTitle:'小邱港货2周年啦！'}
 function shareFriend(){WeixinJSBridge.invoke('sendAppMessage',{"appid":shareInfo.appid,"img_url":shareInfo.imgUrl,"img_width":"250","img_height":"250","link":shareInfo.lineLink,"desc":shareInfo.descContent,"title":shareInfo.shareTitle},function(res){})}
 function shareTimeline(){WeixinJSBridge.invoke('shareTimeline',{"img_url":shareInfo.imgUrl,"img_width":"200","img_height":"200","link":shareInfo.lineLink,"desc":shareInfo.descContent,"title":shareInfo.shareTitle},function(res){});}
 function shareWeibo(){WeixinJSBridge.invoke('shareWeibo',{"content":shareInfo.descContent,"url":shareInfo.lineLink,},function(res){});}
